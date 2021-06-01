d1f4f8eacc4dba5745488541b49ce7dff9279762
sanyogvarshney12 <sanyogvarshney12@gmail.com>
2021-06-01 20:57:10 +0530

    added logging config and helper file

---
+++ src/main/java/com/airport/logger/ApplicationLogger.java
@@ -0,0 +1,46 @@
+package com.airport.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Sanyog Varshney
+ * @since 1.0
+ * @version 1.0
+ * @apiNote LOGGER CONFIG
+ */
+public class ApplicationLogger {
+
+    private final Logger logger = LoggerFactory.getLogger(ApplicationLogger.class);
+
+    /**
+     *
+     * @param className
+     * @param methodName
+     * @param msg
+     */
+    public void debug(String className, String methodName, String msg) {
+        logger.debug("CLASSNAME : {} :: METHODNAME : {} :: {}", className, methodName, msg);
+    }
+
+    /**
+     *
+     * @param className
+     * @param methodName
+     * @param msg
+     * @param obj
+     */
+    public void debug(String className, String methodName, String msg, Object obj) {
+        logger.debug("CLASSNAME : {} :: METHODNAME : {}  :: {} {}", className, methodName, msg, obj);
+    }
+
+    /**
+     *
+     * @param msg
+     * @param e
+     */
+    public void error(String msg, Throwable e) {
+        logger.error(msg, e);
+    }
+
+}
---
+++ src/main/java/helper/PropertyHelper.java
@@ -0,0 +1,38 @@
+package helper;
+
+import com.airport.logger.ApplicationLogger;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Sanyog Varshney
+ * @since 1.0
+ * @version 1.0
+ * @implNote utility class
+ */
+public class PropertyHelper {
+    private static final Properties properties = new Properties();
+    private static final String CLASSNAME = PropertyHelper.class.getName();
+    private static final ApplicationLogger logger = new ApplicationLogger();
+
+    private PropertyHelper() {
+        logger.debug(CLASSNAME, "propertyhelper()","constructure instansiated");
+    }
+    static {
+        try {
+            properties.load(PropertyHelper.class
+                    .getClassLoader()
+                    .getResourceAsStream("application.properties"));
+        }catch(IOException e){
+            logger.error("Some System Exception occured", e);
+        }
+    }
+    public static String getProperty(String key){
+        if(properties.containsKey(key)) {
+            return properties.getProperty(key);
+        }else{
+            return "";
+        }
+    }
+}
